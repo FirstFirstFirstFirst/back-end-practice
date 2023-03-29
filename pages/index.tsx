f295897457803b32ab550a968313bd5db38fb28b
unknown <hocrux1st.first@gmail.com>
2023-03-29 18:07:40 +0700

    remove error file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,37 @@
-import AddTodo from '@/components/addTodo'
-import Auth from '@/components/auth'
-import TodoList from '@/components/todoList'
+// import AddTodo from '@/components/addTodo'
+// import Auth from '@/components/auth'
+// import TodoList from '@/components/todoList'
+// import React from 'react'
+// import { ToastContainer } from 'react-toastify'
+// export default function App() {
+//  return (
+//    <div className='w-3/5 font-inter mx-auto mt-4 shadow-lg p-8'>
+//      <h1 className='text-2xl'>Todo @ Firestore</h1>
+//      <br />
+
+
+//      <Auth />
+
+
+//      <br /> <hr className='w-full h-2 bg-red-500' /><br />
+//      <h1 className='text-2xl mb-4'>Add Task</h1>
+//      <AddTodo />
+
+
+//      <br /> <hr className='w-full h-2 bg-red-500' /><br />
+//      <h1 className='text-2xl'>Task list</h1>
+//      <TodoList />
+//      <ToastContainer />
+
+//    </div>
+//  )
+// }
 import React from 'react'
-import { ToastContainer } from 'react-toastify'
-export default function App() {
- return (
-   <div className='w-3/5 font-inter mx-auto mt-4 shadow-lg p-8'>
-     <h1 className='text-2xl'>Todo @ Firestore</h1>
-     <br />
 
-
-     <Auth />
-
-
-     <br /> <hr className='w-full h-2 bg-red-500' /><br />
-     <h1 className='text-2xl mb-4'>Add Task</h1>
-     <AddTodo />
-
-
-     <br /> <hr className='w-full h-2 bg-red-500' /><br />
-     <h1 className='text-2xl'>Task list</h1>
-     <TodoList />
-     <ToastContainer />
-
-   </div>
- )
+const hey = () => {
+  return (
+    <div>hey</div>
+  )
 }
+
+export default hey
